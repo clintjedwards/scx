8182e4d45ef4461b41cc2ef941e0384d0e06e66d
Clint J Edwards <clint.j.edwards@gmail.com>
2024-07-12 20:22:29 -0400

    test 2

--- rust/scx_rustland_core/assets/bpf/main.bpf.c
+++ rust/scx_rustland_core/assets/bpf/main.bpf.c
@@ -1041,6 +1041,6 @@ SCX_OPS_DEFINE(rustland,
 	       .init			= (void *)rustland_init,
 	       .exit			= (void *)rustland_exit,
 	       .flags			= SCX_OPS_ENQ_LAST | SCX_OPS_KEEP_BUILTIN_IDLE,
-	       .timeout_ms		= 5000,
+	       .timeout_ms		= 500000,
 	       .dispatch_max_batch	= MAX_DISPATCH_SLOT,
 	       .name			= "rustland");
